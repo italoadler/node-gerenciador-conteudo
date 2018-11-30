3c3daea1b10dd0f2140d77bd8879df06ac68d10c
franciscovernek <franciscovernek@hotmail.com>
2018-11-29 21:47:53 -0300

    Rotas (Refactory)
    - Alteração nos retornos das rotas

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var app = require('./config/server')
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+var rotaHome = require('./app/routes/home')(app);
 
-var rotaPublicacoes = require('./app/routes/publicacoes');
-rotaPublicacoes(app);
+var rotaPublicacoes = require('./app/routes/publicacoes')(app);
 
-var rotaNovaPublicacao = require('./app/routes/nova-publicacao');
-rotaNovaPublicacao(app);
+var rotaNovaPublicacao = require('./app/routes/nova-publicacao')(app);
 
 app.listen(3000, function () {
     console.log('Servidor rodando...');
